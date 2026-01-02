8e465d7ce0fc6abaf7774e3ea3cafb212ebf029e
gushiii <1726143054@qq.com>
2026-01-02 16:14:51 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, rust!");
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        // The assert_eq! macro checks if two values are equal
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn another_test() {
+        // This test will fail because panic! is called
+        // panic!("Make this test fail");
+    }
 }
